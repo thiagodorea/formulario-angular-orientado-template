0588c4622cf4b1f4282935ddd09a2cf242c387d6
thiagodorea <thiago.dorea@hotmail.com>
2023-03-25 12:04:05 -0300

    projeto inicial

---
+++ src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,21 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms'
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.component.html',
+  styleUrls: ['./cadastro.component.css']
+})
+export class CadastroComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  cadastrar(form: NgForm){
+    form.valid ? this.router.navigate(['./sucesso']) : alert('Formulario inválido.');
+    console.log(form);
+  }
+}
